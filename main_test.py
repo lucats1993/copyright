3dc15fcb2c8c8a3651220862e21e7ea41d6ec1f3
wangll <1459435619@qq.com>
2018-01-18 14:25:05 +0800

    增加多线程

--- main_test.py
+++ main_test.py
@@ -1,30 +1,58 @@
 #coding:utf-8
 import os
 from co_name import CoName
-#path='C:/Users/wll/Desktop/simple/2017-09-14-16h35m17s'#文件夹目录source_test
-path='C:/Users/wll/Desktop/simple/test'
-if __name__ == '__main__':
-    file_names = os.listdir(path)  # 得到文件夹下的所有文件名称
-    i=0
-    for file in file_names:
-        file_path =path+'/'+file
-        with open(file_path, 'r') as f:
-            html=f.read()
-            try:
-                co = CoName(html)
-                copyright=co.get_copyright()
-                # if copyright:
-                #     copyright =copyright.decode('utf-8')[:150].encode('utf-8')
-                co_name =co.get_co_name(copyright)
-            except Exception as e:
-                print e
-            else:
-                if copyright:
-                    print co_name
-                    # print("%s %s"%(copyright,file_path))
-                    i=i+1
-    print i
+import time
+import random
+from multiprocessing import Process,Queue,Pool,Manager
+path="D:/document/simple/2017-08-22-17h09m39s"
+def write(q):
+    print('Process to write: %s' % os.getpid())
+    for value in write_html():
+        # print('Put %s to queue...' % value)
+        q.put(value)
+        # print q.get(True)
+        # time.sleep(random.random())
 
+def write_html():
+    # 得到文件夹下的所有文件名称
+    file_path = path
+    for file_name in os.listdir(file_path):
+        with open(os.path.join(path, file_name), 'r') as f:
+            yield f.read()
+
+def deal(html):
+    try:
+        co = CoName(html)
+        copyright = co.get_copyright()
+        # urls = co.get_url()
+
+    except Exception as e:
+        print e
+    else:
+        if copyright:
+            print copyright
+
+def deal_html(q):
+    print('Process to read: %s' % os.getpid())
+    while True:
+        if not q.empty():
+            value = q.get(True)
+            deal(value)
+        else:
+            break
+
+
+if __name__ == '__main__':
+    manager = Manager()
+    t = time.time()
+    q = manager.Queue()
+    p = Pool()
+    pw = p.apply_async(write,args=(q,))
+    for i in range(1):
+        p.apply_async(deal_html, args=(q,))
+    p.close()
+    p.join()
+    print time.time()-t
 
 
 
